b62b57c9ef4e47268162228f0b03b9d8dec69140
Alexander Oonk <alexanderoonk26@gmail.com>
2023-09-29 13:16:27 +0200

    added ortec identifiers, but id's are wrong...

--- databallpy/load_data/event_data/ortec.py
+++ databallpy/load_data/event_data/ortec.py
@@ -13,6 +13,140 @@ from databallpy.warnings import DataBallPyWarning
 import json
 
 
+ORTEC_EVENTS_MAP = {
+   0: "Unknown",
+   1: "Action",
+   2: "ActionSuccesfull",
+   3: "Passes",
+   4: "PassCompleted",
+   5: "PassForward",
+   6: "PassFowardCompleted",
+   7: "PassWide",
+   8: "PassWideCompleted",
+   9: "PassBackward",
+   10: "PassBackwardCompleted",
+   11: "PassLongForward",
+   12: "PassShort",
+   13: "PassFirstInPossession",
+   14: "PassForwardFirstInPossession",
+   15: "PassBetweenCentralDefenders",
+   16: "PassOpponentHalf",
+   17: "PassInFinalThird",
+   18: "PassInFinalThirdCompleted",
+   19: "PassCross",
+   20: "PassCrossEarly",
+   21: "PassCrossLate",
+   22: "PassToOpponentPenaltyBox",
+   23: "PassDirect",
+   24: "ActionKey",
+   25: "CornerKeyAction",
+   26: "FreeKickKeyAction",
+   27: "ActionAssist",
+   28: "GoalAttemptScored",
+   29: "GoalWithHead",
+   30: "Goalattempt",
+   31: "GoalAttemptOnTarget",
+   32: "ShotInsideOpponentPenaltyBox",
+   33: "ShotInsideOpponentPenaltyBoxScored",
+   34: "ShotInsideOpponentPenaltyBoxOnTarget",
+   35: "ShotOutsideOpponentPenaltyBox",
+   36: "ShotOutsideOpponentPenaltyBoxScored",
+   37: "ShotOutsideOpponentPenaltyBoxOnTarget",
+   38: "FreeKickOnGoal",
+   39: "ShotWithHead",
+   40: "PossessionRegainByDuel",
+   41: "Interception",
+   42: "PossessionRegainInPlayOpponentHalf",
+   43: "PossessionLoss",
+   44: "PossessionLossByPass",
+   45: "PossessionLossByDuel",
+   46: "ActionClearance",
+   47: "ActionSliding",
+   48: "DuelDefendingPartGroundStanding",
+   49: "DuelDefendingPartGroundStandingWon",
+   50: "DuelDefendingPartAir",
+   51: "DuelDefendingPartAirWon",
+   52: "DuelDefendingPart",
+   53: "DuelWonByDefender",
+   54: "DuelAttackingPartGroundStanding",
+   55: "DuelAttackingPartGroundStandingWon",
+   56: "DuelAttackingPartAir",
+   57: "DuelAttackingPartAirWon",
+   58: "DuelAttackingPart",
+   59: "DuelWonByAttacker",
+   60: "DuelDefendingPartOpponentHalf",
+   61: "DuelDefendingPartOpponentHalfWon",
+   62: "DuelDefendingPartOwnHalf",
+   63: "DuelDefendingPartOwnHalfWon",
+   64: "Offside",
+   65: "Dribble",
+   66: "Corner",
+   67: "ThrowIn",
+   68: "Foul",
+   69: "SaveOnGoalAttempt",
+   70: "FreeKick",
+   71: "KeeperThrowShort",
+   72: "KeeperThrowLong",
+   73: "PassSwitchOffPlay",
+   74: "PassInBox",
+   75: "DribbleSuccesfull",
+   76: "ActionOpponentBox",
+   77: "BlockedShot",
+   78: "CornerShort",
+   79: "CornerLong",
+   80: "Possession",
+   81: "PossessionOppHalf",
+   82: "ThrowinKeepPossession",
+   83: "FoulSuffered",
+   84: "Penalty",
+   85: "FiftyFiftyDuel",
+   87: "TotalXLocation",
+   88: "TotalYLocation",
+   89: "Grade",
+   90: "Speed",
+   91: "PossessionTime",
+   92: "PassesOwnHalf",
+   93: "PassesOwnHalfCompleted",
+   94: "PassesOpponentHalfCompleted",
+   95: "Keypasses",
+   96: "PassCrossCompleted",
+   97: "PassCrossHigh",
+   98: "PassCrossLow",
+   99: "PassCrossToGoalAttempt",
+   100: "PassCrossToGoal",
+   101: "DirectFreekickScored",
+   102: "DirectFreekkickMissed",
+   103: "AssistsByFreekick",
+   104: "PenaltyScored",
+   105: "DefensiveDuelsOwnBox",
+   106: "DefensiveDuelsOwnboxWon",
+   107: "PossessionRegainInPlay",
+   108: "PossessionRegainInPlayOwnHalf",
+   109: "InterceptionByGoalkeeper",
+   111: "GoalKicks",
+   112: "YellowCards",
+   113: "RedCards",
+   114: "DirectRedCards",
+   115: "FoulsOwnHalf",
+   116: "OwnGoals",
+   117: "DefensiveBlocks",
+   118: "PassLongForwardCompleted",
+   119: "DuelsTotal",
+   120: "DuelsTotalWon",
+}
+
+ORTEC_DATABALLPY_MAP = {}
+for ortec_event in ORTEC_EVENTS_MAP.values():
+    if "pass" in ortec_event.lower() or "corner" in ortec_event.lower() or "throw" in ortec_event.lower() or "cross" in ortec_event.lower():
+        ORTEC_DATABALLPY_MAP[ortec_event] = "pass"
+    elif "goal" in ortec_event.lower() or "shot" in ortec_event.lower():
+        ORTEC_DATABALLPY_MAP[ortec_event] = "shot"
+    elif "dribble" in ortec_event.lower() or "duelattackingpartground" in ortec_event.lower():
+        ORTEC_DATABALLPY_MAP[ortec_event] = "dribble"
+    
+
+ORTEC_COMPLETED_PROPERTY_ID = 8
+
 def load_ortec_event_data(
     event_data_loc: str, metadata_loc: str
 ) -> Tuple[pd.DataFrame, Metadata]:
@@ -35,7 +169,11 @@ def load_ortec_event_data(
     metadata = _load_metadata(metadata_loc)
     metadata, possessions = _update_metadata(metadata, event_data_loc)
     event_data = _load_ortec_event_data(event_data_loc=event_data_loc)
-    return event_data, metadata, databallpy_events
+    home_mask = event_data["team_id"] == metadata.home_team_id
+    away_mask = event_data["team_id"] == metadata.away_team_id
+    event_data.loc[home_mask, "player_name"] = event_data.loc[home_mask, "player_id"].map(metadata.home_players.set_index("id")["full_name"])
+    event_data.loc[away_mask, "player_name"] = event_data.loc[away_mask, "player_id"].map(metadata.away_players.set_index("id")["full_name"])
+    return event_data, metadata, {}
 
 def _update_metadata(metadata: Metadata, event_data_loc: str) -> Tuple[Metadata, pd.DataFrame]:
     """Function that updates the metadata with the event data. The function 
@@ -89,13 +227,48 @@ def _load_ortec_event_data(event_data_loc: str) -> pd.DataFrame:
         "start_y": [],
         "datetime": [],
         "ortec_event": [],
+        "home_score": [],
+        "away_score": [],
+        "to_player_id": [],
+        "to_team_id": [],
+        "end_x": [],
+        "end_y": [],
     }
+    to_add_minutes = {1: 0, 2: 45, 3: 90, 4: 105, 5: 120}
+
     for event in data["Events"]:
+        result_dict["event_id"].append(event["Id"])
+        result_dict["period_id"].append(event["Phase"])
+        milliseconds = event["Time"] 
+        seconds = milliseconds / 1000
+        result_dict["minutes"].append(seconds // 60 + to_add_minutes[event["Phase"]])
+        result_dict["seconds"].append(seconds % 60)
+        result_dict["datetime"].append(pd.to_datetime(event["DateTime"], utc=True))
+        result_dict["home_score"].append(event["Score"][0])
+        result_dict["away_score"].append(event["Score"][1])
+        annot = event["Annotations"][0]
+        result_dict["player_id"].append(annot["PersonId"] if "PersonId" in annot.keys() else MISSING_INT)
+        result_dict["team_id"].append(annot["SelectionEditionId"] if "SelectionEditionId" in annot.keys() else MISSING_INT)
+        result_dict["type_id"].append(annot["Label"]) if "Label" in annot.keys() else MISSING_INT
+        event_name = ORTEC_EVENTS_MAP[annot["Label"]] if "Label" in annot.keys() else "Unknown"
+        result_dict["ortec_event"].append(event_name)
+        result_dict["start_x"].append(annot["LocationX"] if "LocationX" in annot.keys() else np.nan)
+        result_dict["start_y"].append(annot["LocationY"] if "LocationY" in annot.keys() else np.nan)
+        result_dict["to_player_id"].append(annot["NextPersonId"] if "NextPersonId" in annot.keys() else MISSING_INT)
+        result_dict["to_team_id"].append(annot["NextSelectionEdition"] if "NextSelectionEdition" in annot.keys() else MISSING_INT)
+        result_dict["end_x"].append(annot["NextLocationX"] if "NextLocationX" in annot.keys() else np.nan)
+        result_dict["end_y"].append(annot["NextLocationY"] if "NextLocationY" in annot.keys() else np.nan)
+        if len(annot["Properties"]) > 0:
+            result_dict["outcome"].append([0, 1][ORTEC_COMPLETED_PROPERTY_ID in annot["Properties"]])
+        else:
+            result_dict["outcome"].append(MISSING_INT)
 
-    
-   
+    result_dict["databallpy_event"] = [None] * len(result_dict["event_id"])
 
-        import pdb; pdb.set_trace()
+    df = pd.DataFrame(result_dict)
+    df["databallpy_event"] = df["ortec_event"].map(ORTEC_DATABALLPY_MAP)
+
+    return df
 
 
 def _get_player_info(info_players: list) -> pd.DataFrame:
