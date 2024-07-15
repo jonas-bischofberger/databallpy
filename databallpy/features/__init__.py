f06d7337e2226f081467b5fcf8a0b594c9e430ed
Alexander Oonk <alexanderoonk26@gmail.com>
2024-07-15 14:40:51 +0200

    proposed changes

--- databallpy/features/__init__.py
+++ databallpy/features/__init__.py
@@ -1,5 +1,5 @@
 from databallpy.features.angle import get_smallest_angle
-from databallpy.features.differentiate import add_velocity
+from databallpy.features.differentiate import add_velocity, add_acceleration
 from databallpy.features.pitch_control import (
     get_pitch_control_period,
     get_pitch_control_single_frame,
@@ -8,3 +8,4 @@ from databallpy.features.pitch_control import (
 from databallpy.features.player_possession import get_individual_player_possession
 from databallpy.features.pressure import get_pressure_on_player
 from databallpy.features.team_possession import add_team_possession
+from databallpy.features.covered_distance import get_covered_distance
